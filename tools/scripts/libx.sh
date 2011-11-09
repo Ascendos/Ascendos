7a5e04470088957b81066a8c12c7747696a3c337
Douglas McClendon <dmc@cloudsession.com>
2011-11-09 02:00:28 -0600

    massive dynamics, not very stable, but vast improvements
    - security/keys moved to /infra/security/keys for aesthetics
    - defaults/configfiles: added/migrated many settings, see
                            tools/scripts/defaults for the full list
    - bake: move a lot of target configuration out to defaults/config files
    - bake: refactor update-net-cache into its own script
    - bake: integrate full epel-6 into the input repo dependencies (for now)
    - bake: introduce some bugs trying to get koji to work entirely from caches,
            did much more good than harm
    - bake: refactor uncomma_list utility function
    - bake: pass in more kickstart config files via the entire devtree passing,
      	in addition to defaults/configfile specified paths to those items.
    - bake: added baker vm install repo kernel/initramfs checksum verification
    - {destroy/view}-last-bake-vm: a couple new utility/debugging tools/scripts
    - better docs/ documentation
    - el-push: refactored to use libx.sh for code sharing
    - el-push: support/default 'none' for a signing user means not to sign packages
    - v-bake: added epel to baking vm's yum config, for makeworld/koji at least
    - v-bake: support (nondefault) option to add devtree on the fly to livecd output
    - v-bake: bugfix the (now nondefault) option for rebootless installion
              support on the live builds
    - v-bake: access configs and keys under devtree directly in vm rather
              than downloading
    - v-bake: get f13/epel6 srpms from local net cache
    - v-bake: get zli from cloudsession.com/dawg/downloads/zyx-liveinstaller
              instead of f13
    - v-bake: some experiments with getting grub.x86_64 to build (see bug#74)
    - v-bake: use for koji, byd default input use gen-tdv-srpm-repo generated
              srpms rather than prebuilt from build.ascendos.org
    - v-bake: use configfiles for build-against- targets rather than just
              trying against an ordered list of possible bootstrap_os_repos
    - v-bake: compile-sources: copy mock site-defaults.cfg into place
    - allover: general cleanups and comment modifications

--- tools/scripts/libx.sh
+++ tools/scripts/libx.sh
@@ -29,6 +29,7 @@
 # note: if you'd like a different license, just let me know which and why
 #############################################################################
 
+
 #############################################################################
 #############################################################################
 ##
@@ -41,6 +42,8 @@
 ## libx.sh is just a centralized entrypoint into a set of shared bash scripts
 ## and libraries.
 
+
+#############################################################################
 ##
 ## sanity checks: these must be defined before sourcing libx.sh
 ##
@@ -53,6 +56,8 @@ if [ "${x_prefix}" == "" ]; then
     exit 1
 fi
 
+
+#############################################################################
 ##
 ## get runtime environment
 ##
@@ -64,6 +69,8 @@ progdir=$( ( pushd $( dirname $( readlink -e "${0}" ) ) > /dev/null 2>&1 ; \
 rundir=$( pwd )
 mypid=$$
 
+
+#############################################################################
 ##
 ## explicitly set tmpdir to default if not set
 ##
@@ -71,40 +78,65 @@ if [ "${TMPDIR}" == "" ]; then
     export TMPDIR="/tmp"
 fi
 
+
+#############################################################################
 ##
 ## load libraries
 ##
 if [ -f "${progdir}/../../tools/scripts/libx.sh" ]; then
-    source "${progdir}/../../tools/scripts/defaults"
-    source "${progdir}/../../tools/scripts/functions"
-    source "${progdir}/../../tools/scripts/common"
     x_devenv=1
     x_devdir=$( ( pushd "${progdir}/../.." > /dev/null 2>&1 ; \
 	pwd ; popd > /dev/null 2>&1 ) )
+    x_libdir="${x_devdir}/tools/scripts"
     export PATH="${x_devdir}/tools/bin:${x_devdir}/tools/scripts:${PATH}"
 elif [ -f "/root/output/devtree/Ascendos/tools/scripts/libx.sh" ]; then
-    # sad sad tired workaround.  Better to somehow run v-bake as v-bake
-    # within the extracted full devtree (with options passing)
-    source  "/root/output/devtree/Ascendos/tools/scripts/defaults"
-    source  "/root/output/devtree/Ascendos/tools/scripts/functions"
-    source  "/root/output/devtree/Ascendos/tools/scripts/common"
-    source  "/tmp/v-bake.options"
     x_devenv=1
     x_devdir=/root/output/devtree/Ascendos
+    x_libdir="${x_devdir}/tools/scripts"
     export PATH="${x_devdir}/tools/bin:${x_devdir}/tools/scripts:${PATH}"
 elif [ -f "${x_prefix}/lib/${x_toolname}/scripts/libx.sh" ]; then
-    source  "${x_prefix}/lib/${x_toolname}/scripts/defaults"
-    source  "${x_prefix}/lib/${x_toolname}/scripts/functions"
-    source  "${x_prefix}/lib/${x_toolname}/scripts/common"
+    # TODO: actually support and test this case.  
+    # - need toplevel 'make sources' and 'make rpm' etc...
     x_devenv=0
     x_libdir="${x_prefix}/lib/${x_toolname}"
-    export PATH="${x_prefix}/lib/${x_toolname}/tools/bin:${x_prefix}/lib/${x_toolname}/tools/scripts:${PATH}"
+    export PATH="${x_prefix}/lib/${x_toolname}/scripts:${PATH}"
 else
     echo "${progname}: fatal error trying to find and load libx.sh library"
     exit 1
 fi
 
+
 #############################################################################
+##
+## get/set configuration options/variables
+##
+
+# source/set package configuration defaults 
+source "${x_libdir}/defaults"
+
+# source/set local tree configuration overrides if they exist
+if [ -f "${x_libdir}/localconfig" ]; then
+    source "${x_libdir}/localconfig"
+fi
+
+# source/set local user configuration overrides if they exist
+if [ -f "${HOME}/.el-build/config" ]; then
+    source "${HOME}/.el-build/config"
+fi
+
+
+#############################################################################
+##
+## common library code
+##
+
+# import common functions
+source "${x_libdir}/functions"
+
+# source common script
+source "${x_libdir}/common"
+
+
 #############################################################################
 #############################################################################
 #############################################################################
